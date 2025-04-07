aa977ff89e2edf22f3ec1a419216ae7d42ab10e7
stapperu <k.kubiak94@gmail.com>
2025-04-07 15:46:03 +0200

    ' added color/difficulty option, some visual fluff to colors, msgs..'

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const squares = document.querySelectorAll(".square");
 const levelLabel = document.querySelector(".levelLabel");
 const message = document.querySelector(".message");
 const startButton = document.querySelector(".startButton");
+const statusMsg=document.querySelector(".statusMsg");
+const harderDiff=document.querySelector(".harder");
+const normalDiff=document.querySelector(".normal");
+const docBody=document.querySelector("body");
+const square1=document.querySelector('.square1')
+const square2=document.querySelector('.square2')
+const square3=document.querySelector('.square3')
+const square4=document.querySelector('.square4')
+
 
 let temporaryArray = [];
 let memorizedArray = [];
@@ -9,7 +18,7 @@ let levelNumber=1;
 
 function addToMemorized() {
   const randomSquareSelect = Math.floor(Math.random() * 4 + 1);
-  squares[randomSquareSelect - 1].style.border = "5px solid black";
+  squares[randomSquareSelect - 1].style.border = "5px solid white";
   setTimeout(() => {
     squares[randomSquareSelect - 1].style.border = "none";
   }, 2000);
@@ -50,25 +59,28 @@ setTimeout(() => {
     
     if (isMatch) {
       // Full match: Success
-      message.textContent = "Success";
+      statusMsg.textContent = "Success";
       levelNumber++;
       levelLabel.textContent="Level " + levelNumber;
       setTimeout(() => {
-        message.textContent = "";}, 2000);
+        statusMsg.textContent = "";
         addToMemorized();
         clearTemporary();
-       
+      }, 1000);
       
     } else if (temporaryArray.length <= memorizedArray.length) {
       // Check for mismatch only within temporaryArray's length
       for (let i = 0; i < temporaryArray.length; i++) {
         if (temporaryArray[i] !== memorizedArray[i]) {
-          message.textContent = "Fail";
+          statusMsg.style.color='red';
+          statusMsg.textContent = "Fail";
+           levelLabel.textContent="Level 1"
           setTimeout(() => {
-            message.textContent = "";}, 2000);
+            statusMsg.textContent = "";
+            statusMsg.style.color='white';
             clearMemorized();
             clearTemporary();
-          
+          }, 1000);
           
           break; // Exit after failing
         }
@@ -79,7 +91,24 @@ setTimeout(() => {
 );
 
 startButton.addEventListener("click", () => {
+   statusMsg.textContent='Game Started, follow the sequence'
+  setTimeout(() => {
+    statusMsg.textContent=''}, 1000);
   clearTemporary();
   clearMemorized();
   addToMemorized();
 });
+
+
+harderDiff.addEventListener('click', ()=>{ docBody.style.backgroundColor='black';
+  square1.style.backgroundColor="silver";
+  square2.style.backgroundColor="gray";
+  square3.style.backgroundColor="lightGray";
+  square4.style.backgroundColor="darkGray";
+})
+
+normalDiff.addEventListener('click', ()=>{ docBody.style.backgroundColor='rgb(204, 231, 198)';
+  square1.style.backgroundColor="green";
+  square2.style.backgroundColor="blue";
+  square3.style.backgroundColor="red";
+  square4.style.backgroundColor="yellow";})
